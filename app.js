5a8af2f4d91f41aa619d6696e45ef08ff17c008d
Brian Harris <brianharrisdesign@gmail.com>
2017-11-08 18:52:11 -0800

    0 validation but totally working

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 var express = require("express");
+var cors = require('node-cors');
 var app = express();
+var morgan = require("morgan");
 var bodyParser = require("body-parser");
 var nodemailer = require('nodemailer');
 
-app.post('/:name', function(request, response) {
+// var whiteList = ['http://localhost:3000'];
+// app.use(cors());
+
+// middleware
+app.set("view engine", "pug");
+app.use(morgan("tiny"))
+app.use(bodyParser.urlencoded({extended:true}));
+// parse application/json
+app.use(bodyParser.json())
+
+// CORS headers
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
+app.post('/contact', function(req, res, next) {
+  // eval(require("locus"))
+  if (Object.keys(req.body).length === 0)
+    res.send(JSON.stringify("Request Body was Empty"));
+
   var transporter = nodemailer.createTransport({
     service: 'Gmail',
     auth: {
@@ -12,11 +38,11 @@ app.post('/:name', function(request, response) {
     }
   })
 
-  var emailSubject = `Portfolio Contact from: ${request.body.fullName}`;
-  var emailBody = `From: ${request.body.fullName}\n
-                  Email: ${request.body.email}\n
-                  Subject: ${request.body.subject}\n
-                  Body: ${request.body.emailBody}`;
+  var emailSubject = `Portfolio Contact from: ${req.body.fullName}`;
+  var emailBody = `From: ${req.body.fullName}\n
+                  Email: ${req.body.email}\n
+                  Subject: ${req.body.subject}\n
+                  Body: ${req.body.body}`;
 
   var mailOptions = {
     from: 'brianharris.dev@gmail.com', // hide this in environ var
@@ -31,10 +57,41 @@ app.post('/:name', function(request, response) {
       res.json({msg: 'error while sending email'});
     } else {
       console.log('Message sent: ' + info.response);
-      response.json({msg: info.response});
+      res.json({msg: info.response});
     }
   });
 })
+
+// // catch 404 and forward to error handler
+// app.use(function(req, res, next) {
+//   var err = new Error('Not Found');
+//   err.status = 404;
+//   next(err);
+// });
+
+// // error handlers
+
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//   app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.render('error', {
+//       message: err.message,
+//       error: err
+//     });
+//   });
+// }
+
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: {}
+//   });
+// });
 
 app.listen(5000, function() {
   console.log("The server has started on port 5000");
