16645e87a64c7bd5ba0f71d7e3369bb6820dd065
Brian Harris <brianharrisdesign@gmail.com>
2017-11-09 14:08:51 -0800

    now it really does use env variables

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ require('dotenv').config();
 
 // middleware
 app.set("view engine", "pug");
-app.use(morgan("tiny"))
+// app.use(morgan("tiny"))
 app.use(bodyParser.urlencoded({extended:true}));
 // parse application/json
 app.use(bodyParser.json())
 
 // CORS headers
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", '*');
+  res.header("Access-Control-Allow-Origin", process.env.AUTH_DOMAIN);
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
@@ -23,7 +23,6 @@ app.use(function(req, res, next) {
 });
 
 app.post('/contact', function(req, res, next) {
-  // eval(require("locus"))
   if (Object.keys(req.body).length === 0)
     res.send(JSON.stringify("Request Body was Empty"));
 
@@ -49,46 +48,15 @@ app.post('/contact', function(req, res, next) {
   }
 
   transporter.sendMail(mailOptions, function(err, info) {
-    if (error) {
-      console.log(error);
-      res.json({msg: 'error while sending email'});
+    if (err) {
+      console.log(err);
+      res.json({msg: 'FAIL'});
     } else {
       console.log('Message sent: ' + info.response);
       res.json({msg: info.response});
     }
   });
 })
-
-// // catch 404 and forward to error handler
-// app.use(function(req, res, next) {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// // error handlers
-
-// // development error handler
-// // will print stacktrace
-// if (app.get('env') === 'development') {
-//   app.use(function(err, req, res, next) {
-//     res.status(err.status || 500);
-//     res.render('error', {
-//       message: err.message,
-//       error: err
-//     });
-//   });
-// }
-
-// // production error handler
-// // no stacktraces leaked to user
-// app.use(function(err, req, res, next) {
-//   res.status(err.status || 500);
-//   res.render('error', {
-//     message: err.message,
-//     error: {}
-//   });
-// });
 
 app.listen(5000, function() {
   console.log("The server has started on port 5000");
