639363929d52c00e686f0ba8eab4bf16b687bbb0
Brian Harris <brianharrisdesign@gmail.com>
2017-11-25 16:54:56 -0800

    newrelic config file moved and values added

---
+++ newrelic.js
@@ -0,0 +1,25 @@
+'use strict'
+/**
+ * New Relic agent configuration.
+ *
+ * See lib/config.defaults.js in the agent distribution for a more complete
+ * description of configuration variables and their potential values.
+ */
+exports.config = {
+  /**
+   * Array of application names.
+   */
+  app_name: ['Node Mail Relay'],
+  /**
+   * Your New Relic license key.
+   */
+  license_key: '340e9884dae0d59e56290db328999a456c73faad',
+  logging: {
+    /**
+     * Level at which to log. 'trace' is most useful to New Relic when diagnosing
+     * issues with the agent, 'info' and higher will impose the least overhead on
+     * production applications.
+     */
+    level: 'info'
+  }
+}
